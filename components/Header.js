6b23d7b5bb731f9f306d8490f229674a7e5f6354
Melanie <git@melanie-busse.de>
2026-02-09 12:03:36 +0100

    3. Insert syling

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,28 @@
 
 export default function Header () {
   return (
-    <header>
+    <header className="header-styles"> {/* Füge CSS-Klasse hinzu */}
       <h1>Recap 5</h1>
+      <nav className="nav-styles">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a href="#spotlight" className="nav-link">
+              Spotlight
+              <svg className="arrow" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M7 10l5 5 5-5z"/>
+              </svg>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#gallery" className="nav-link">
+              Gallery
+              <svg className="arrow" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M7 10l5 5 5-5z"/>
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
