f677cc98f54415e3d55367cc391c3bc164376856
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-02-10 18:30:36 +0100

    add inicomment pattern

---
+++ components/initComment.js
@@ -0,0 +1,10 @@
+export const initComments = [
+  {
+    slug: "orange-red-and-green",
+    isFavorite : false,
+    comments: [
+      { comment: "nice art", date: "10-02-2026" },
+      { comment: "greates one", date: "10-02-2026" }, 
+    ],
+  },
+];
